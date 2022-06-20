52e5ee2ce41f2be7d66e31d7159a23e680982130
Ayush <bitturaikwar@gmail.com>
2022-06-20 21:21:44 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Blog from "./components/Blog";
+import Heading from "./components/Heading";
+import Layout from "./components/Layout";
+import List from "./components/List";
+import Navbar from "./components/Navbar";
+import { main } from "./styles/homeStyle";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Navbar />
+      <hr style={{ marginTop: "3rem", color: "black", size: "2px" }} />
+
+      <Heading />
+
+      <hr style={{ marginTop: "3rem", color: "black", size: "2px" }} />
+
+      <div style={main}>
+        <Routes>
+          <Route path="/" element={<Blog />} />
+          <Route path="/:id" element={<Blog />} />
+        </Routes>
+
+        <List />
+      </div>
+    </Layout>
   );
-}
+};
 
 export default App;
